f9defaf4ef7a2233449224935563aa3d2d5b3f02
giorgi17 <giorgi199817@gmail.com>
2019-12-02 05:11:35 +0400

    Basic MVC template

---
+++ Models/indexModel.php
@@ -0,0 +1,18 @@
+<?php
+/** Autoloading The required Classes **/
+class IndexModel {
+
+function __construct(){
+
+}
+
+public function getUsers(){
+  $users = [
+  ["name" => "Williams Isaac", "Phone Number" => "090982xxxxxx"],
+  ["name" => "Oji Mike", "Phone Number"=> "080982xxxxxx"]
+  ];
+  return json_encode($users);
+}
+
+}
+?>
